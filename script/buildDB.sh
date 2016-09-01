126b9ef4ff1ed01e880a7873554778b779eea06c
CelioBarros <celio.barros.filho@gmail.com>
2016-09-01 15:35:23 -0300

    Removendo arquivos desnecessários

--- script/buildDB.sh
+++
@@ -1,6 +0,0 @@
-echo "Iniciando script BuildDB"
-mysql -h db-insa -uroot -p < db_insa.sql
-echo "Tabelas criadas"
-Rscript Rscript.R
-echo "Banco povoado!"
-
--- script/run.sh
+++
@@ -1,5 +0,0 @@
-echo "PEGANDO OS DADOS"
-sh get_dados.sh
-echo "COLETA DE DADOS FINALIZADA. TRANSFORMANDO OS DADOS EM UM CSV"
-python parser.py "dados/*.html" "reservatorios.csv"
-echo "FIM"
