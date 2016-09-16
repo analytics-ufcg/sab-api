9e17ab23db6bf22217f2ea62966ee55517fde898
CelioBarros <celio.barros.filho@gmail.com>
2016-09-16 16:45:37 -0300

    Nova função para verificar se o dado é ou não ruido

--- script/insert_month_on_DB.py
+++ script/insert_month_on_DB.py
@@ -7,6 +7,7 @@ import MySQLdb
 from datetime import datetime
 from dateutil import relativedelta
 import time
+import numpy
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -41,9 +42,10 @@ def insert_many_BD(values):
 	cursor.close()
 	conn.close()
 
-# ADICIONAR VALORES DA CONSULTA DO MONITORAMENTO PARA QUANDO FOR INSERIR VERIFICAR SE VAI COMPARAR COM O ANTERIOR OU COM ELE
-def define_picos(lista_reserv, ultimo_monitoramento):
 
+# ADICIONAR VALORES DA CONSULTA DO MONITORAMENTO PARA QUANDO FOR INSERIR VERIFICAR SE VAI COMPARAR COM O ANTERIOR OU COM ELE
+def retira_ruido(lista_reserv, ultimo_monitoramento):
+	diasRange = relativedelta.relativedelta(days=60)
 	if(not ultimo_monitoramento[3] is None):
 		ultimo_monitamento_lista = list(ultimo_monitoramento)
 		data_lista = ultimo_monitamento_lista[4].split("-")
@@ -53,39 +55,50 @@ def define_picos(lista_reserv, ultimo_monitoramento):
 		lista_reservatorios.extend(list(reversed(lista_reserv)))
 	else:
 		lista_reservatorios= list(reversed(lista_reserv))
-	ultimoValorValido = None
-	porcentagem = 25
+
 	for m in range(len(lista_reservatorios)):
-		if((m == 0)and (m+1 < len(lista_reservatorios)) and (ultimo_monitoramento[3] is None)):
-			# VERIFICANDO SE ESTÁ FORA DO INTERVALO DE 60 DIAS
-			if((datetime.strptime(lista_reservatorios[m][4], "%Y-%m-%d") <= (datetime.strptime(lista_reservatorios[m+1][4], "%Y-%m-%d")- relativedelta.relativedelta(days=60)))
-				# VERIFICANDO SE ESTÁ DENTRO DA MARGEM DE 30% PRA MAIS OU PRA MENOS NA PORCENTAGEM
-				or ((float(lista_reservatorios[m][3]) >= (float(lista_reservatorios[m+1][3])-porcentagem)) and (float(lista_reservatorios[m][3]) <= (float(lista_reservatorios[m+1][3])+porcentagem)))):
-				ultimoValorValido = m
-				lista_reservatorios[m][5] = 1
-
-		elif(m > 0):
-			if(ultimoValorValido is None):
-				# VERIFICANDO SE ESTÁ FORA DO INTERVALO DE 60 DIAS
-				if((datetime.strptime(lista_reservatorios[m][4], "%Y-%m-%d") >= (datetime.strptime(lista_reservatorios[m-1][4], "%Y-%m-%d") + relativedelta.relativedelta(days=60)))
-				# VERIFICANDO SE ESTÁ DENTRO DA MARGEM DE 30% PRA MAIS OU PRA MENOS NA PORCENTAGEM
-					or ((float(lista_reservatorios[m][3]) >= (float(lista_reservatorios[m-1][3])-porcentagem)) and (float(lista_reservatorios[m][3]) <= (float(lista_reservatorios[m-1][3])+porcentagem)))):
-					ultimoValorValido = m
-					lista_reservatorios[m][5] = 1
-
+		valores = []
+		valoresAnteriores = []
+		valoresPosteriores = []
+		porcentagemAtual = float(lista_reservatorios[m][3])
+		for reserv in range(len(lista_reservatorios)):
+			# VERIFICA SE ESTÁ DENTRO DO VALOR DO RANGE
+			if((datetime.strptime(lista_reservatorios[m][4], "%Y-%m-%d") <= (datetime.strptime(lista_reservatorios[reserv][4], "%Y-%m-%d")+ diasRange))
+				and ((datetime.strptime(lista_reservatorios[m][4], "%Y-%m-%d") >= (datetime.strptime(lista_reservatorios[reserv][4], "%Y-%m-%d")- diasRange)))):
+				# VERIFICA SE O VALOR É ANTERIOR OU POSTERIOR
+				if(m > reserv):
+					if(int(lista_reservatorios[reserv][5]) == 1):
+						valoresAnteriores.append(float(lista_reservatorios[reserv][3]))
+				else:
+					valoresPosteriores.append(float(lista_reservatorios[reserv][3]))
+		# VERIFICA SE TEM A MESMA QUANTIDADE DE VALORES ANTERIORES E POSTERIORES
+		if(len(valoresAnteriores) == len(valoresPosteriores) or len(valoresAnteriores) == 0 or len(valoresPosteriores) == 0):
+			valores.extend(valoresAnteriores)
+			valores.extend(valoresPosteriores)
+		elif(len(valoresAnteriores) < len(valoresPosteriores)):
+			valores.extend(valoresAnteriores)
+			for i in range(len(valoresAnteriores)):
+				valores.append(valoresPosteriores[i])
+		else:
+			for i in range(len(valoresPosteriores), -1, -1):
+				valores.append(valoresAnteriores[i])
+			valores.extend(valoresPosteriores)
+		# VERIFICA SE O DADO NÃO É ISOLADO
+		if(len(valores) > 0):
+			desvioPadrao = numpy.std(valores)
+			if (desvioPadrao>30):
+				intervalo = desvioPadrao
+			else:	
+				intervalo = desvioPadrao+10
+			media = numpy.mean(valores)
+			if((porcentagemAtual <= media+intervalo) and (porcentagemAtual >= media-intervalo)):
+				lista_reservatorios[m].append(1)
 			else:
-				# VERIFICANDO SE ESTÁ FORA DO INTERVALO DE 60 DIAS
-				if(datetime.strptime(lista_reservatorios[m][4], "%Y-%m-%d") >= (datetime.strptime(lista_reservatorios[ultimoValorValido][4], "%Y-%m-%d") + relativedelta.relativedelta(days=60))
-					# VERIFICANDO SE ESTÁ DENTRO DA MARGEM DE 30% PRA MAIS OU PRA MENOS NA PORCENTAGEM
-					or ((float(lista_reservatorios[m][3]) >= (float(lista_reservatorios[ultimoValorValido][3])-porcentagem)) and 
-						(float(lista_reservatorios[m][3]) <= (float(lista_reservatorios[ultimoValorValido][3])+porcentagem)))):
-					ultimoValorValido = m
-					lista_reservatorios[m][5] = 1
-	if(not ultimo_monitoramento[3] is None):
-		lista_reservatorios.pop(0)
+				lista_reservatorios[m].append(0)
+		else:
+			lista_reservatorios[m].append(1)
+	
 	return lista_reservatorios
-
-
 
 
 ultimos_monitoramentos = consulta_BD("SELECT mon.id, mo.cota, mo.volume, mo.volume_percentual, date_format(mo.data_informacao,'%d-%m-%Y') FROM tb_monitoramento mo RIGHT JOIN (SELECT r.id, max(m.data_informacao) AS maior_data FROM tb_reservatorio r LEFT OUTER JOIN tb_monitoramento m ON r.id=m.id_reservatorio GROUP BY r.id) mon ON mo.id_reservatorio=mon.id AND mon.maior_data=mo.data_informacao;")
@@ -126,11 +139,11 @@ for monitoramento in ultimos_monitoramentos:
 				volumePercentual = json_insert["VolumePercentual"].replace(",", ".")
 				dataInformacao = datetime.strptime(json_insert["DataInformacao"], formato_data_1).strftime(formato_data_3)
 				id_reservatorio = int(json_insert["Codigo"])
-				to_insert.append([id_reservatorio,cota,volume,volumePercentual,dataInformacao,0])
+				to_insert.append([id_reservatorio,cota,volume,volumePercentual,dataInformacao])
 
 			json_insert={}
 
 	if(len(to_insert) >0):
-		insert_many_BD(define_picos(to_insert, monitoramento))
+		insert_many_BD(retira_ruido(to_insert, monitoramento))
 		
 	time.sleep(1)
