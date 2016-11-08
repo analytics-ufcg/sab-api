fe47f6839d9506f9f9da2d69227883f8be299821
CelioBarros <celio.barros.filho@gmail.com>
2016-11-08 14:33:38 -0300

    inserindo a uf no resumo do estado

--- api_mandacaru.py
+++ api_mandacaru.py
@@ -172,7 +172,7 @@ def reservoirs_equivalent_hydrographic_basin():
 
 
 def reservoirs_equivalent_states():
-	query = ("SELECT es.nome AS estado, ROUND(SUM(info.volume),2) AS volume_equivalente, ROUND(SUM(info.capacidade),2) AS capacidade_equivalente,"
+	query = ("SELECT es.nome AS estado,es.sigla AS sigla, ROUND(SUM(info.volume),2) AS volume_equivalente, ROUND(SUM(info.capacidade),2) AS capacidade_equivalente,"
 		" ROUND((SUM(info.volume)/SUM(info.capacidade)*100),2) AS porcentagem_equivalente, COUNT(DISTINCT info.id_reservatorio) AS quant_reservatorio_com_info,"
 		" (COUNT(DISTINCT res.id)-COUNT(DISTINCT info.id_reservatorio)) AS quant_reservatorio_sem_info ,COUNT(DISTINCT res.id) AS total_reservatorios,"
 		" CAST(SUM(CASE WHEN info.volume_percentual <= 10 THEN 1 ELSE 0 END) as UNSIGNED) AS intervalo_1,"
@@ -186,11 +186,11 @@ def reservoirs_equivalent_states():
 		" WHERE m.data_informacao >= (CURDATE() - INTERVAL 90 DAY) GROUP BY m.id_reservatorio) info_data"
 		" WHERE info_data.id_reserv=mo.id_reservatorio AND re.id=mo.id_reservatorio AND mo.data_informacao=info_data.data_info) info"
 		" ON info.id_reservatorio=res.id INNER JOIN tb_reservatorio_municipio rm ON rm.id_reservatorio=res.id INNER JOIN tb_municipio mu"
-		" ON rm.id_municipio=mu.id INNER JOIN tb_estado es ON es.id=mu.id_estado GROUP BY es.nome;")
+		" ON rm.id_municipio=mu.id INNER JOIN tb_estado es ON es.id=mu.id_estado GROUP BY es.nome, es.sigla;")
 
 	select_answer = IO.select_DB(query)
 
-	keys = ["estado", "volume_equivalente","capacidade_equivalente", "porcentagem_equivalente", "quant_reservatorio_com_info","quant_reservatorio_sem_info",
+	keys = ["estado", "uf", "volume_equivalente","capacidade_equivalente", "porcentagem_equivalente", "quant_reservatorio_com_info","quant_reservatorio_sem_info",
 	 "total_reservatorios", "quant_reserv_intervalo_1", "quant_reserv_intervalo_2", "quant_reserv_intervalo_3", "quant_reserv_intervalo_4",
 	  "quant_reserv_intervalo_5"]
 
