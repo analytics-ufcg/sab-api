61618a76c8216391fa7ba18863d04aa183df7d75
igormeira <igor.meira@gmail.com>
2017-06-30 09:56:03 -0300

    Refatoramento

--- resources.py
+++ resources.py
@@ -131,39 +131,35 @@ def api():
 def states_sab():
 	response = json.dumps(api_mandacaru.states_sab())
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
+	#response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 @app.route('/api/pais')
 def json_brazil():
 	response = json.dumps(api_mandacaru.json_brazil())
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 @app.route('/api/reservatorios')
 def reservoirs():
 	response = json.dumps(api_mandacaru.reservoirs())
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 @app.route('/api/reservatorios/info')
 @app.route('/api/reservatorios/<id>/info')
 def reservoirs_information(id=None):
-	if (id is None):
-		response = json.dumps(api_mandacaru.reservoirs_information())
-	else:
-		response = json.dumps(api_mandacaru.reservoirs_information(int(id)))
+    if (id is None):
+        response = json.dumps(api_mandacaru.reservoirs_information())
+    else:
+        response = json.dumps(api_mandacaru.reservoirs_information(int(id)))
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 @app.route('/api/reservatorios/<id>/monitoramento')
 def reservoirs_monitoring(id):
 	response = json.dumps(api_mandacaru.reservoirs_monitoring(int(id),False))
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 @app.route('/api/reservatorios/<id>/monitoramento/csv')
@@ -174,7 +170,6 @@ def reservoirs_monitoring_csv(id):
 	cw = csv.writer(si)
 	cw.writerows(csvList)
 	response = make_response(si.getvalue())
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	response.headers["Content-Disposition"] = "attachment; filename=monitoramento_" + id + ".csv"
 	response.headers["Content-type"] = "text/csv"
 	return response
@@ -183,14 +178,12 @@ def reservoirs_monitoring_csv(id):
 def reservoirs_monitoring_complete(id):
 	response = json.dumps(api_mandacaru.reservoirs_monitoring(int(id),True))
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 @app.route('/api/reservatorios/similares/<nome>/<limiar>')
 def reservoirs_similar(nome, limiar):
 	response = json.dumps(api_mandacaru.reservoirs_similar(nome, int(limiar)))
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 
@@ -198,7 +191,6 @@ def reservoirs_similar(nome, limiar):
 def reservoirs_equivalent_hydrographic_basin():
 	response = json.dumps(api_mandacaru.reservoirs_equivalent_hydrographic_basin())
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 
@@ -206,26 +198,22 @@ def reservoirs_equivalent_hydrographic_basin():
 def reservoirs_equivalent_states():
 	response = json.dumps(api_mandacaru.reservoirs_equivalent_states())
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 @app.route('/api/municipios/sab')
 def city_info():
 	response = json.dumps(api_mandacaru.city_info(1))
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 @app.route('/api/municipios')
 def city_info_brazil():
 	response = json.dumps(api_mandacaru.city_info(0))
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
 
 @app.route('/api/pesquisa/municipio_reservatorio')
 def search_information():
 	response = json.dumps(api_mandacaru.search_information())
 	response = make_response(response)
-	response.headers['Access-Control-Allow-Origin'] = "*"
 	return response
