d92eab354542240ca7c95703463c8589c17c3e2e
igormeira <igor.meira@gmail.com>
2017-08-01 18:10:25 -0300

    Calculo demanda

--- predict/aux_predict_info.py
+++ predict/aux_predict_info.py
@@ -34,23 +34,35 @@ def rowsToList(rows):
         return lista
 
 def demanda_dif(reservatId, data, volumeAtual):
-    lastWeek = data - timedelta(days=7)
-    query = """SELECT volume, data_informacao FROM tb_monitoramento WHERE id_reservatorio="""+str(reservatId)+""" AND
-            data_informacao='"""+str(lastWeek)+"""' GROUP BY volume """
-    rows = rowsToList(aux_collection_insert.consulta_BD(query))
+    #lastWeek = data - timedelta(days=7)
+    #query = """SELECT volume, data_informacao FROM tb_monitoramento WHERE id_reservatorio="""+str(reservatId)+""" AND
+    #        data_informacao='"""+str(lastWeek)+"""' GROUP BY volume """
+    #rows = rowsToList(aux_collection_insert.consulta_BD(query))
 
-    if len(rows) <= 0:
-            query = """SELECT volume, MAX(data_informacao) FROM tb_monitoramento
-                    WHERE id_reservatorio="""+str(reservatId)+""" AND
-                    data_informacao=(SELECT MAX(data_informacao)
-                                    FROM tb_monitoramento
-                                    WHERE id_reservatorio="""+str(reservatId)+""") GROUP BY volume"""
-            rows = rowsToList(aux_collection_insert.consulta_BD(query))
+    #if len(rows) <= 0:
 
-    dif_datas = data - rows[1]
+    #query = """SELECT volume, MAX(data_informacao) FROM tb_monitoramento
+    #    WHERE id_reservatorio="""+str(reservatId)+""" AND
+    #        data_informacao=(SELECT MAX(data_informacao)
+    #                        FROM tb_monitoramento
+    #                        WHERE id_reservatorio="""+str(reservatId)+""") GROUP BY volume"""
+    #rows = rowsToList(aux_collection_insert.consulta_BD(query))
+
+    query_datas = """SELECT MIN(data_informacao), MAX(data_informacao) FROM tb_monitoramento WHERE id_reservatorio="""+str(reservatId)+""" AND
+            data_informacao BETWEEN '2017-01-01' AND '2017-03-01'"""
+    rows_datas = rowsToList(aux_collection_insert.consulta_BD(query_datas))
+
+    query_fim = """SELECT volume, data_informacao FROM tb_monitoramento WHERE id_reservatorio="""+str(reservatId)+""" AND
+            data_informacao='"""+str(rows_datas[0])+"""'"""
+    rows_fim = rowsToList(aux_collection_insert.consulta_BD(query_fim))
+
+    query_inicio = """SELECT volume, data_informacao FROM tb_monitoramento WHERE id_reservatorio="""+str(reservatId)+""" AND
+            data_informacao='"""+str(rows_datas[1])+"""'"""
+    rows_inicio = rowsToList(aux_collection_insert.consulta_BD(query_inicio))
+
+    dif_datas = rows_datas[0] - rows_datas[1]
     day = dif_datas.days
-    media_dem = ((float(rows[0]) * 10000.00) - float(volumeAtual)) / day
-
+    media_dem = ((float(rows_fim[0]) * 10000.00) - (float(rows_inicio[0]) * 10000.00)) / day
     return media_dem if media_dem > 0.0 else 0.0
 
 def critical(reservatId):
--- predict/predict.py
+++ predict/predict.py
@@ -7,7 +7,6 @@ def calcula(list_of_values):
     reservatId = list_of_values[0]
 
     volume_atual = float(list_of_values[10]) * 1000000.00
-    volume_critico = aux_predict_info.critical(reservatId)
 
     data = list_of_values[12]
     data = datetime.strptime(data, "%d/%m/%Y").date()
@@ -16,12 +15,11 @@ def calcula(list_of_values):
     predict_info.popular_variaveis(reservatId)
     dias = 0
 
-    while (volume_atual > volume_critico):
-        print volume_atual
-        previsao = predict_info.volumeParcial(reservatId, mes, volume_atual) + predict_info.precip() + predict_info.vazao() \
-                    - predict_info.evap(reservatId, mes) - predict_info.demanda(reservatId, data, volume_atual)
+    demanda = predict_info.demanda(reservatId, data, volume_atual)
+
+    while (volume_atual > 0.0):
+        previsao = predict_info.volumeParcial(reservatId, mes, volume_atual) + predict_info.precip() + predict_info.vazao() - demanda
         volume_atual = previsao
         dias += 1
 
-    print volume_critico
     return dias
