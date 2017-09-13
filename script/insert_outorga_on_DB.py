8d092fdc64a5d806de050c718006a4c91c176f2e
igormeira <igor.meira@gmail.com>
2017-09-13 17:19:57 -0300

    Mudança nas outorgas

--- script/insert_outorga_on_DB.py
+++ script/insert_outorga_on_DB.py
@@ -29,40 +29,60 @@ def nomes_PB():
     rows = aux_collection_insert.consulta_BD(query)
     return rows
 
+def check_data(acude, listaDados):
+	for i in range(0, len(outorgas[acude])):
+		if outorgas[acude][i][1] == listaDados[0][1] and outorgas[acude][i][2] == listaDados[0][2] and outorgas[acude][i][3] == listaDados[0][3] and outorgas[acude][i][4] == listaDados[0][4]:
+			return i
+	return None
+
+def sum_vazao(acude):
+	vazao = 0
+	for listaInterna in outorgas[acude]:
+		vazao += listaInterna[1]
+	return vazao
+
 def insert_outorga(outorgas_pb, resertvats):
-    today = date.today()
-    for res in resertvats:
-        acude = outorgas_pb["Açude Monitorados"][0]
-        expira = outorgas_pb["Expiração da Outorga"][0]
-        vazao = outorgas_pb["Vazão Horária  (m³/h)"][0]
+	today = date.today()
+	for res in resertvats:
+		acude = outorgas_pb["Açude Monitorados"][0]
+		expira = outorgas_pb["Expiração da Outorga"][0]
+		vazao = outorgas_pb["Vazão Horária  (m³/h)"][0]
+		latitude = outorgas_pb["Latitude"][0]
+		longitude = outorgas_pb["Longitude"][0]
+		uso = outorgas_pb["Tipo de Uso"][0]
+		listaDados = []
 
-        if remove_accents(acude) == remove_accents(res[0]) and expira != "null" and vazao != "null":
-            data = datetime.strptime(outorgas_pb["Expiração da Outorga"][0], "%d/%m/%Y").date()
-            if data.year >= 2010:
-                vazao = float(vazao) * 24.00
-                if outorgas.get(acude) != None:
-                    if data > today and outorgas[acude][0] > today:
-                        vazao = outorgas[acude][1] + vazao
-                        global outorgas
-                        outorgas[acude] = [data, vazao]
-                    elif data > outorgas[acude][0]:
-                        global outorgas
-                        outorgas[acude] = [data, vazao]
-                else:
-                    global outorgas
-                    outorgas[acude] = [data, vazao]
-                query = """UPDATE tb_reservatorio SET outorga="""+str(outorgas[acude][1])+""" WHERE id="""+str(res[1])
-                aux_collection_insert.update_BD(query)
+		if remove_accents(acude) == remove_accents(res[0]) and expira != "null" and vazao != "null":
+			data = datetime.strptime(outorgas_pb["Expiração da Outorga"][0], "%d/%m/%Y").date()
+			vazao = float(vazao) * 24.00
+			listaDados.append([data, vazao, latitude, longitude, remove_accents(uso)])
 
-def popular_outorga():
-    reader_outorgas_pb = csv.DictReader(open('../data/outorgas_pb.csv'))
+			if outorgas.get(acude) != None:
+				index = check_data(acude, listaDados)
+				if index != None:
+					if outorgas[acude][index][0] < listaDados[0][0]:
+						global outorgas
+						outorgas[acude][index] = listaDados[0]
+				else:
+					global outorgas
+					outorgas[acude].append(listaDados[0])
+			else:
+				global outorgas
+				outorgas[acude] = [[data, vazao, latitude, longitude, remove_accents(uso)]]
+
+			vazao_total = sum_vazao(acude)
+			query = """UPDATE tb_reservatorio SET outorga="""+str(vazao_total)+""" WHERE id="""+str(res[1])
+			aux_collection_insert.update_BD(query)
+
+#def popular_outorga():
+reader_outorgas_pb = csv.DictReader(open('../data/outorgas_pb.csv'))
+outorgas_pb = {}
+resertvats = nomes_PB()
+for row in reader_outorgas_pb:
+    for column, value in row.iteritems():
+        outorgas_pb.setdefault(column, []).append(value)
+    insert_outorga(outorgas_pb, resertvats)
     outorgas_pb = {}
-    resertvats = nomes_PB()
-    for row in reader_outorgas_pb:
-        for column, value in row.iteritems():
-            outorgas_pb.setdefault(column, []).append(value)
-        insert_outorga(outorgas_pb, resertvats)
-        outorgas_pb = {}
 
 def create_outorga():
     query = """ALTER TABLE tb_reservatorio ADD COLUMN outorga MEDIUMTEXT NULL AFTER demanda"""
