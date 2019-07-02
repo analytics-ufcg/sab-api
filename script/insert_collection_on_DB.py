ceb914989e7ac830c5c4fa424d04790b836032dc
samirsm <samir.silva.medeiros@ccc.ufcg.edu.br>
2019-07-02 10:14:41 -0300

    corrigindo script ana

--- script/insert_collection_on_DB.py
+++ script/insert_collection_on_DB.py
@@ -6,10 +6,13 @@ import requests
 from datetime import datetime
 import time
 import aux_collection_insert
+import urllib3
 
 
 reload(sys)
 sys.setdefaultencoding('utf8')
+urllib3.disable_warnings()
+
 
 ultimos_monitoramentos = aux_collection_insert.consulta_BD("SELECT mon.id, mo.cota, mo.volume, mo.volume_percentual, date_format(mo.data_informacao,'%d-%m-%Y') FROM tb_monitoramento mo RIGHT JOIN (SELECT r.id, max(m.data_informacao) AS maior_data FROM tb_reservatorio r LEFT OUTER JOIN tb_monitoramento m ON r.id=m.id_reservatorio GROUP BY r.id) mon ON mo.id_reservatorio=mon.id AND mon.maior_data=mo.data_informacao;")
 
@@ -34,12 +37,17 @@ for monitoramento in ultimos_monitoramentos:
 	else:
 		data_inicial = str(monitoramento[4])
 
-	r = requests.get('http://sar.ana.gov.br/Medicao?dropDownListReservatorios='+reserv+'&dataInicial='+data_inicial+'&dataFinal='+data_final+'&button=Buscar')
+	try:
+		r = requests.get('https://www.ana.gov.br/sar0/Medicao?dropDownListReservatorios='+reserv+'&dataInicial='+data_inicial+'&dataFinal='+data_final+'&button=Buscar', verify=False)
+	except requests.exceptions.RequestException as e:  # This is the correct syntax
+		print e
+		continue	
 
 	json_insert = {}
 	contador_coluna = 0
 
 	soup = BeautifulSoup(r.text.encode('utf8'), 'html.parser')
+	print(soup)
 	for link in soup.find_all('td'):
 		if(contador_coluna < len(cabecalho)):
 			json_insert[cabecalho[contador_coluna]] = link.get_text().strip()
